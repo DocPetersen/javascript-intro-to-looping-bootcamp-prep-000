a97234c9086e3cdf4b1e5278d100a512824ca3d3
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-08-25 05:56:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,6 @@
 function forLoop(array) {
-  for (var i = 0; i < 25; i++)
-{
-  if (i === 1);
-  {
+  for (var i = 0; i < 25; i++) {
+  if (i === 1) {
     array.push("I am ${i} strange loop");
   } 
   else 
